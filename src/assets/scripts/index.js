0d069fc21e98a050a9b1ba3df56a1ff0aff779fe
Azhari Subroto <juarathemes@gmail.com>
2021-01-01 07:44:31 +0700

    initial commit

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,7 @@
+// import { qs, qsa, $on, $delegate } from './utils';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../stylesheets/style.scss';
+import 'bootstrap/js/dist/dropdown';
+
+// console.log('Hello!');
